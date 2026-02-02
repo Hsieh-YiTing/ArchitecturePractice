9458272d7ddf364fda42739ed86a3e4e1d2433f9
Ryan Hsieh <ryan.hsieh@chungpeng.com.tw>
2026-02-02 15:26:45 +0800

    feat: 新增Dapper套件進行查詢；新增DI的介面套件進行依賴集中管理

--- ArchitecturePractice.Repositories/ExportReport/ExportReportDropdownRepository.cs
+++ ArchitecturePractice.Repositories/ExportReport/ExportReportDropdownRepository.cs
@@ -1,9 +1,48 @@
-﻿using ArchitecturePractice.Core.ExportReport.Interface;
+﻿using ArchitecturePractice.Core.ExportReport.DropdownModel;
+using ArchitecturePractice.Core.ExportReport.Interface;
+using Dapper;
+using System.Data;
 
 namespace ArchitecturePractice.Repositories.ExportReport
 {
-    public class ExportReportDropdownRepository : IExportReportDropdownRepository
+    public class ExportReportDropdownRepository(IDbConnection dbConnection) : IExportReportDropdownRepository
     {
+        private readonly IDbConnection _dbConnection = dbConnection;
 
+        /// <summary>
+        /// 公司選單ID、公司名稱查詢語法。
+        /// </summary>
+        private const string GetCompanyItemsQuery = @"
+            SELECT 
+            CM01 AS Id, 
+            CM02 AS CompanyName
+            FROM Company
+            WHERE CM003 != 1
+            ORDER BY CM01";
+
+        /// <summary>
+        /// 依據公司ID查詢一級單位ID、一級單位名稱查詢語法。
+        /// </summary>
+        private const string GetRolesByCompanyIdQuery = @"
+            SELECT 
+            CR01 AS Id,
+            CR02 AS RoleName
+            FROM CompanyRole
+            WHERE CM01 = @CompanyId 
+            AND CR03 = 1
+            AND CR003 != 1
+            ORDER BY CR01";
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<CompanyForDropDownModel>> GetCompanyItemsAsync()
+        {
+            return await _dbConnection.QueryAsync<CompanyForDropDownModel>(GetCompanyItemsQuery);
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<RoleForDropDownModel>> GetRoleItemsByCompanyIdAsync(int companyId)
+        {
+            return await _dbConnection.QueryAsync<RoleForDropDownModel>(GetRolesByCompanyIdQuery, new { CompanyId = companyId });
+        }
     }
 }
---
+++ ArchitecturePractice.Repositories/RepositoryDependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using ArchitecturePractice.Core.ExportReport.Interface;
+using ArchitecturePractice.Repositories.ExportReport;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+
+namespace ArchitecturePractice.Repositories
+{
+    public static class RepositoryDependencyInjection
+    {
+        /// <summary>
+        /// 集中管理Repository的註冊。
+        /// </summary>
+        /// <param name="services">擴充方法。</param>
+        /// <returns>IServiceCollection，支援鏈式調用。</returns>
+        public static IServiceCollection AddRepository(this IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<IDbConnection>(sp => new SqlConnection(connectionString));
+            services.AddScoped<IExportReportDropdownRepository, ExportReportDropdownRepository>();
+            return services;
+        }
+    }
+}
